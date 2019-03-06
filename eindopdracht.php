edd7eb52452bda35d5bcc1e172464c08a89fe928
Eefkje-Femke <40299927+Eefkje-Femke@users.noreply.github.com>
2019-03-06 09:37:55 +0100

    add bootstrap files
    bootstrap files added to GitHub.

---
+++ eindopdracht.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="nl">
+	<head>
+		<meta charset="UTF-8">
+		<title>Eindopdracht bootstrap</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link rel="stylesheet" href="bootstrap.css"><!--mijn css file-->
+	</head>
+  <body>
+		<div class="container">
+		</div>
+    <!--js-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="bootstrap.js"></script>
+  </body>
+</html>
