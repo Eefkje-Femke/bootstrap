7e65ad03dea2b7381a1abec4f25cf5f4c1045000
Eefkje-Femke <40299927+Eefkje-Femke@users.noreply.github.com>
2019-03-13 08:58:30 +0100

    end-result-Files
    This is my bootstrap website! Enjoy!

--- eindopdracht.php
+++ eindopdracht.php
@@ -6,8 +6,6 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="shortcut icon" type="image/png" href="images/sigma.png"/>
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-		<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> -->
-		<!-- <link rel="stylesheet" href="/plugins/bootstrap/css/bootstrap.min.css"> -->
 		<link rel="stylesheet" href="/plugins/bootstrap/css/bootstrap.css"><!--plugins-->
 		<link rel="stylesheet" href="bootstrap.css"><!--mijn css file-->
 	</head>
